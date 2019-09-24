275ce2bbd3d617bc0d2b8cf9d339c49f0243b376
Emad <emal02002@utb.vaxjo.se>
2019-09-24 13:55:11 +0200

    har gjort lite av uppg

---
+++ metod 2/metod 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace metod_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            byt(ref a, ref b);
+            Console.WriteLine(a + " " + b);
+
+        }
+        public static void byt(ref int a, ref int b)
+        {
+            int temp = a;
+            b = a;
+            b = temp;
+        }
+    }
+}
