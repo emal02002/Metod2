b517baf4f78fc27a14d895a240d228fea75ca37b
Emad <emal02002@utb.vaxjo.se>
2019-09-27 09:46:23 +0200

    har börjatr med uppgift 2

---
+++ metod del2 uppgift 2/metod del2 uppgift 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace metod_del2_uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange en bas");
+            int bas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ange en exponenet");
+            int exponent = int.Parse(Console.ReadLine());
+            power(bas, exponent);
+            Console.WriteLine(power(bas, exponent));
+         }
+
+
+        static public int power(int b ,int e)
+        {
+            return (b * e);
+        }
+
+    }
+}
